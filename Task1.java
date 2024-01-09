46f07c0a92a5b6c11dcf2631db9370fa80596d7f
syedmasoodrizvi <125534543+syedmasoodrizvi@users.noreply.github.com>
2024-01-09 16:18:34 +0500

    Update Task1.java

--- Task1.java
+++ Task1.java
@@ -6,7 +6,7 @@ public class Task1 {
 		int[] number = {25,33,43,81,78,41,381,191,201};
 		try{
 			System.out.println(number[1]);
-			System.out.println(number[40]);
+			System.out.println(number[41]);
 			}
 			catch(ArrayIndexOutOfBoundsException e){ 
 				System.out.println ("Array Index is Out Of Bounds");
